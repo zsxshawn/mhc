cfc6f4b1e1f31ea9548da5ed1027dc7c60cc5f46
Shawn24 <78519750+zsxshawn@users.noreply.github.com>
2024-06-27 17:56:42 -0400

    Update NetMHCpan.py

--- NetMHCpan.py
+++ NetMHCpan.py
@@ -4,22 +4,30 @@ import mhctools
 from mhctools import NetMHCpan, NetMHCIIpan
 from mhcnames import normalize_allele_name
 
+if len(sys.argv) < 2:
+    print("Error: No tool specified. Please specify either 'NetMHCpan' or 'NetMHCIIpan'.")
+    sys.exit(1)
+
+tool = sys.argv[1]
+
 print("Python version:", sys.version)
 print("Pandas version:", pd.__version__)
 print("mhctools version:", mhctools.__version__)
 
-# Test creation of NetMHCpan and NetMHCIIpan predictors to check versions
-try:
-    pan_predictor = NetMHCpan(alleles=[normalize_allele_name("HLA-A*02:01")])
-    print("NetMHCpan loaded successfully.")
-except Exception as e:
-    print(f"Error loading NetMHCpan: {e}")
-
-try:
-    ii_pan_predictor = NetMHCIIpan(alleles=[normalize_allele_name("HLA-DRB1*01:01")])
-    print("NetMHCIIpan loaded successfully.")
-except Exception as e:
-    print(f"Error loading NetMHCIIpan: {e}")
+if tool == "NetMHCpan":
+    try:
+        pan_predictor = NetMHCpan(alleles=[normalize_allele_name("HLA-A*02:01")])
+        print("NetMHCpan loaded successfully.")
+    except Exception as e:
+        print(f"Error loading NetMHCpan: {e}")
+elif tool == "NetMHCIIpan":
+    try:
+        ii_pan_predictor = NetMHCIIpan(alleles=[normalize_allele_name("HLA-DRB1*01:01")])
+        print("NetMHCIIpan loaded successfully.")
+    except Exception as e:
+        print(f"Error loading NetMHCIIpan: {e}")
+else:
+    print("Error: Invalid tool specified. Please specify either 'NetMHCpan' or 'NetMHCIIpan'.")
+    sys.exit(1)
 
 print("All modules imported successfully!")
-print("From Github")
