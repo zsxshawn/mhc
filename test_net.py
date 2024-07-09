49a200e3b974c615983580dd617f5210247ea05e
Shawn24 <78519750+zsxshawn@users.noreply.github.com>
2024-07-09 11:20:38 -0400

    Update test_net.py

--- test_net.py
+++ test_net.py
@@ -1,93 +1,24 @@
 from mhctools import NetMHCpan
 
-DEFAULT_ALLELE = 'HLA-A*02:01'
+# Run NetMHCpan for alleles HLA-A*01:01 and HLA-A*02:01
+predictor = NetMHCpan(alleles=["A*02:01", "hla-a0101"])
 
-protein_sequence_dict = {
-    "SMAD4-001": "ASIINFKELA",
-    "TP53-001": "ASILLLVFYW"
+# Scan the short proteins 1L2Y and 1L3Y for epitopes
+protein_sequences = {
+    "1L2Y": "NLYIQWLKDGGPSSGRPPPS",
+    "1L3Y": "ECDTINCERYNGQVCGGPGRGLCFCGKCRCHPGFEGSACQA"
 }
 
-# Tests will also be run using the following program names if they are installed.
-# In any case, a program called "netMHCpan" MUST be installed and working for
-# this test suite to succeed.
-OPTIONAL_NETMHCPAN_PROGRAM_NAMES = [
-    "netMHCpan-2.8",
-    "netMHCpan-3.0",
-    "netMHCpan-4.0",
-    "netMHCpan-4.1",
-]
+# Predict binding for subsequences of specified lengths
+binding_predictions = predictor.predict_subsequences(protein_sequences, peptide_lengths=[9])
 
-# Custom MHC sequences in FASTA format
-custom_mhc_sequences = """>MHC1
-MSAQRVGSLADGRTVEALHGAEGLRQSLPDC
->MHC2
-MSLQRVGSLADGRTVEALHGAEGLRQSLPDC
-"""
+# Flatten binding predictions into a Pandas DataFrame
+df = binding_predictions.to_dataframe()
 
-def test_netmhc_pan():
-    check_netmhc_pan("netMHCpan", True)  # required
+# Print the DataFrame
+print(df)
 
-    for program_name in OPTIONAL_NETMHCPAN_PROGRAM_NAMES:
-        check_netmhc_pan(program_name, False)  # optional
-
-def check_netmhc_pan(program_name, fail_if_no_such_program=True):
-    try:
-        predictor = NetMHCpan(
-            alleles=[DEFAULT_ALLELE], program_name=program_name)
-    except FileNotFoundError:
-        if fail_if_no_such_program:
-            raise
-        print("Skipping because no such program: %s" % program_name)
-        return
-
-    binding_predictions = predictor.predict_subsequences(
-        protein_sequence_dict,
-        peptide_lengths=[9])
-    assert len(binding_predictions) == 4, \
-        "Expected 4 binding predictions from %s" % (binding_predictions,)
-    for x in binding_predictions:
-        offset = x.offset
-        length = x.length
-        seq_name = x.source_sequence_name
-        expected_peptide = protein_sequence_dict[seq_name][offset:offset + length]
-        eq_(expected_peptide, x.peptide,
-            "Peptide mismatch: expected %s but got %s in binding prediction '%s'" % (
-                expected_peptide, x.peptide, x,))
-
-def test_netmhc_pan_custom_mhc():
-    predictor = NetMHCpan(custom_mhc_sequences=custom_mhc_sequences, program_name="netMHCpan-4.1")
-
-    binding_predictions = predictor.predict_subsequences(
-        protein_sequence_dict,
-        peptide_lengths=[9])
-    assert len(binding_predictions) == 4, \
-        "Expected 4 binding predictions from %s" % (binding_predictions,)
-    for x in binding_predictions:
-        offset = x.offset
-        length = x.length
-        seq_name = x.source_sequence_name
-        expected_peptide = protein_sequence_dict[seq_name][offset:offset + length]
-        eq_(expected_peptide, x.peptide,
-            "Peptide mismatch: expected %s but got %s in binding prediction '%s'" % (
-                expected_peptide, x.peptide, x,))
-
-def test_netmhc_pan_multiple_lengths():
-    predictor = NetMHCpan(alleles=["A6801"])
-    binding_predictions = predictor.predict_peptides(
-        ["A" * 8, "A" * 9, "A" * 10, "A" * 11])
-    assert len(binding_predictions) == 4, \
-        "Expected 4 epitopes from %s" % (binding_predictions,)
-
-def test_netmhc_pan_multiple_alleles():
-    alleles = 'A*02:01,B*35:02'
-    predictor = NetMHCpan(
-        alleles=alleles,
-        default_peptide_lengths=[9])
-    sequence_dict = {
-        "SMAD4-001": "ASIINFKELA",
-        "TP53-001": "ASILLLVFYW"
-    }
-    binding_predictions = predictor.predict_subsequences(
-        sequence_dict=sequence_dict)
-    assert len(binding_predictions) == 8, \
-        "Expected 8 binding predictions from %s" % (binding_predictions,)
+# Example: Print strong binders
+for binding_prediction in binding_predictions:
+    if binding_prediction.affinity < 100:
+        print("Strong binder: %s" % (binding_prediction,))
